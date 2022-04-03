c10f33926132c7c607c84bf7b6f5a26648e25e46
binaya sharma <mebinayasharma@gmail.com>
2022-04-03 19:04:20 +0545

    java basic oop

--- src/cotiviti/Inheritance.java
+++ src/cotiviti/Inheritance.java
@@ -2,16 +2,23 @@ package cotiviti;
 
 class Parent {
     int a = 2;
+    int b = 12;
+
+    void show(){
+        System.out.println("Hello World");
+    }
 }
 
 class Child extends Parent {
     int a = 3;
+
 }
 
 public class Inheritance {
     public static void main(String[] args) {
         Child newObj = new Child();
-        System.out.println(newObj.a);
+        System.out.println(newObj.b);
+        newObj.show();
     }
 }
 
