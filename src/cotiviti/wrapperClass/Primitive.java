8fbd65db3b5fe47bcbc5eb5dcd203bda6f284150
binaya sharma <mebinayasharma@gmail.com>
2022-04-04 22:17:58 +0545

    java basic oop

---
+++ src/cotiviti/wrapperClass/Primitive.java
@@ -0,0 +1,14 @@
+package cotiviti.wrapperClass;
+
+import java.sql.SQLOutput;
+
+public class Primitive {
+
+    public static void main(String[] args) {
+        int a = 12;
+        Integer v =  new Integer(20);
+
+        System.out.println(v);
+        System.out.println(a);
+    }
+}
